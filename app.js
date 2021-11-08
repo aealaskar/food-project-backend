809e415bc4ad84ca79513f643d7b6008532199ae
hassan alhassaini <hassandak1d@hotmail.com>
2021-11-08 06:14:57 +0300

    done layout

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 // basic instructions to start a BE project-------------------------------------------
-// yarn init -y  ------ to create a package.json file
-// yarn add global add nodemon  -------- to add nodemon and use scripts yarn start : nodemon to make the app more responsive
-// yarn express to make
-// yarn add cors
-// yarn add mongoose
-// yarn add multer
-// yarn add jsonwebtoken
-// yarn add bcrypt
-// yarn add passport passport-local
-// yarn add passport-jwt
+// yarn init -y  ------- to create a package.json file
+// yarn add global add nodemon  ------- to add nodemon and use scripts yarn start : nodemon to make the app more responsive
+// yarn express ------- to create an express application that we save in app.
+// yarn add cors ------- gives a browser access to the BE we need to use CORS
+// yarn add mongoose ------- manages Data and provides schema validation
+// yarn add multer ------- multer is a middleware that is responsible for uploading files
+// yarn add bcrypt ------- to hash our passwords we will use bcrypt
+// yarn add jsonwebtoken ------- to create a token
+// yarn add passport passport-local ------- we will use a strategy or mechanism from passport called LocalStrategy for authenticating the user
+// yarn add passport-jwt ------- A Passport strategy for authenticating with a JSON Web Token
 
 // changed "main" to app.js in package.json and added script :nodemon
 
 // -------------------------------------------------------------------------------------
 
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`The application is runnong on localhost:${port}`);
+});

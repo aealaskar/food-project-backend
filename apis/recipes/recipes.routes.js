4fe41ccddbb29b4717d4da58fff17051e009994c
ahmad <aealaskar@gmail.com>
2021-11-08 18:49:41 +0300

    unauthorized

--- apis/recipes/recipes.routes.js
+++ apis/recipes/recipes.routes.js
@@ -1,31 +1,32 @@
-const express = require("express")
-const upload = require("../../middleware/multer")
-const passport = require("passport")
-const router = express.Router()
+const express = require("express");
+const upload = require("../../middleware/multer");
+const passport = require("passport");
+const router = express.Router();
 
 const {
-    fetchRecipe,
-    fetchDetailRecipe,
-    recipeListFetch,
+  fetchRecipe,
+  fetchDetailRecipe,
+  recipeListFetch,
 } = require("../recipes/recipes.controllers");
 
+router.param("recipeId", async (req, res, next, recipeId) => {
+  const recipe = await fetchRecipe(recipeId, next);
+  if (recipe) {
+    req.recipe = recipe;
+    next();
+  } else {
+    next({ status: 404, message: "recipe not found!" });
+  }
+});
 
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  recipeListFetch
+);
 
+router.get("/:recipeId", fetchDetailRecipe);
 
-router.param("recipeId", async (req, res, next, recipeId) =>{
-const recipe = await fetchRecipe(recipeId, next);
-if (recipe) {
-    req.recipe = recipe
-    next()
-} else {
-    next({ status: 404, message: "recipe not found!"})
-}
-})
+// router.post()
 
-router.get("/", recipeListFetch)
-
-router.get("/:recipeId", fetchDetailRecipe)
-
-router.post()
-
-module.exports = router
+module.exports = router;

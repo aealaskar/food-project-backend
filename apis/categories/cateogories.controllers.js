5be6ab648310497e3dff9ab315d4e031d5554c07
hassan alhassaini <hassandak1d@hotmail.com>
2021-11-09 17:47:15 +0300

    done recipes started category

--- apis/categories/cateogories.controllers.js
+++ apis/categories/cateogories.controllers.js
@@ -1,9 +1,10 @@
 const Category = require("../../DB/models/Category");
+const Recipe = require("../../DB/models/Recipe");
 
-exports.fetchCategory = async (req, res, next) => {
+exports.fetchCategory = async (categoryId, next) => {
   try {
-    const category = await Category.find();
-    return res.json(category);
+    const category = await Category.findById(categoryId);
+    return category;
   } catch (error) {
     next(error);
   }
@@ -18,6 +19,36 @@ exports.createCategory = async (req, res, next) => {
     const newCategory = await Category.create(req.body);
     await newCategory.populate({ path: "owner", select: "username" });
     return res.status(201).json(newCategory);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.recipeCreate = async (req, res, next) => {
+  // if (!req.user._id.equals(req.category.owner._id))
+
+  // return next({
+  //     status:401,
+  //     message: "you are not the owner",
+  // })}
+  try {
+    if (req.file) {
+      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+    }
+
+    const categoryId = req.params.categoryId;
+    req.body = { ...req.body, category: categoryId };
+
+    const newRecipe = await Recipe.create(req.body);
+    await Category.findOneAndUpdate(
+      {
+        _id: req.params.categoryId,
+      },
+      {
+        $push: { recipes: newRecipe._id },
+      }
+    );
+    return res.status(201).json(newRecipe);
   } catch (error) {
     next(error);
   }
